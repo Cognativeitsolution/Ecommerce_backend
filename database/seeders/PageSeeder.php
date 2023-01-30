bab8e840c3a329474c330df5552ac7968bd0ef8c
Sakhawat Hussain <sakhawat@coginitiveit.com>
2023-01-30 17:43:58 +0500

    Pages seeder modified

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -22,25 +22,33 @@ class PageSeeder extends Seeder
                 'slug'=> 'more-links',
                 'short_description'=> "More Links Short Description",
                 'long_description'=> "More Links Long Description",
-            ]
+            ],
+            [
+                'name'=>'About Us',
+                'title'=>'About Us',
+                'slug'=> 'about-us',
+                'short_description'=> "More Links Short Description",
+                'long_description'=> "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            ],
+            [
+                'name'=>'Privacy Policy',
+                'title'=>'Privacy Policy',
+                'slug'=> 'privacy-policy',
+                'short_description'=> "More Links Short Description",
+                'long_description'=> "More Links Long Description",
+            ],
+            [
+                'name'=>'Terms Conditions',
+                'title'=>'Terms & Conditions',
+                'slug'=> 'terms-conditions',
+                'short_description'=> "More Links Short Description",
+                'long_description'=> "More Links Long Description",
+            ],
 
         ];
 
-        foreach ($pages as $key => $value) {
+        foreach ($pages as $value) {
             Page::create($value);
         }
-
-        $faker = \Faker\Factory::create();
-        for($i = 1; $i<=3; $i++){
-
-            $page = new Page;
-            $page->parent_id = 1 ;
-            $page->name = $faker->name();
-            $page->title = $faker->words(4, true);
-            $page->short_description = $faker->text(80);
-            $page->long_description = $faker->text(1000);
-            $page->save();
-        }
-
     }
 }

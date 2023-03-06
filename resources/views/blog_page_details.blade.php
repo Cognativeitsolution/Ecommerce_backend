2e181ecaa86b8612130ea98875058bd9e371e408
Tahseen Raza <tahseenxgfx@gmail.com>
2023-03-06 17:27:12 +0500

    Removed social icons in blog page.

--- resources/views/blog_page_details.blade.php
+++ resources/views/blog_page_details.blade.php
@@ -27,44 +27,8 @@
                                 @endif
                             @endforeach
                         </div>
-                        <h4>social icon</h4>
-                        <div class="abu_wrpr social">
-                            @if ($settings->facebook_account_linik)
-                            <a href="{{ $settings->facebook_account_linik }}" class="au_item">
-                                <div class="icon fb">
-                                    <i class="fab fa-facebook-f"></i>
-                                </div>
-                                Facebook
-                            </a>
-                            @endif
 
-                            @if ($settings->instagram_account_link)
-                            <a href="{{ $settings->instagram_account_link }}" class="au_item">
-                                <div class="icon insta">
-                                    <i class="fab fa-instagram"></i>
-                                </div>
-                                Instagram
-                            </a>
-                            @endif
-
-                            @if ($settings->google_plus_account_link)
-                            <a href="{{ $settings->google_plus_account_link }}" class="au_item">
-                                <div class="icon google_plus">
-                                    <i class="fab fa-google-plus-g"></i>
-                                </div>
-                                Google+
-                            </a>
-                            @endif
-
-                            @if ($settings->pinterest_account_link)
-                            <a href="{{ $settings->pinterest_account_link }}" class="au_item">
-                                <div class="icon pt">
-                                    <i class="fab fa-pinterest"></i>
-                                </div>
-                                Pinterest
-                            </a>
-                            @endif
-                        </div>
+                        
                     </div>
                 </div>
             </div>
